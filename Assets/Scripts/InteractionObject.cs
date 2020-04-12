fad3cf457db3d77b2941ec67095765fcf534837e
Christina Porter <cporter@bellarmine.edu>
2020-04-11 23:51:24 -0400

    Req Commit

---
+++ Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    bool stolenFrom = false;
+    public void DoSellInteraction(string name, int price)
+    {
+        if (stolenFrom == true)
+            price += 0;
+
+    }
+
+    public void IsStolenFrom()
+    {
+        stolenFrom = true;
+    }
+
+}
