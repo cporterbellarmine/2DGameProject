11b6ca4a60073955821c85ec159fbc7868e28c8e
ElizabethFultz <efultz@bellarmine.edu>
2020-04-13 21:32:35 -0400

    ???

---
+++ Assets/Scripts/Curfew.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curfew : MonoBehaviour
+{
+    public bool curfew;
+    TimeTracker time = GameObject.Find("Clock").AddComponent<TimeTracker>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        curfew = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curfew = time.curfew;
+        if(time.curfew == true)
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+        else
+        {
+            GetComponent<Renderer>().enabled = true;
+        }
+    }
+}
