d06388093a23f210fe23d4d13d7ac23ffac9aadc
Christina Porter <cporter@bellarmine.edu>
2020-04-15 19:27:01 -0400

    PartialMoneyCommit

---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MoneyScript : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI text;
+
+    int money;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+
+        money = 0;
+
+    }
+
+    public void AddMoney(int amt)
+    {
+        money += amt;
+        text.text = $"{money}";
+        System.Console.WriteLine("Money Added");
+    }
+}
